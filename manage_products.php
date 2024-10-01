45640e3d430c5af1262c390af02b036375c35b2f
Devyn <devynchew@gmail.com>
2024-10-01 23:57:02 +0800

    Improve sql query, table responsiveness

--- manage_products.php
+++ manage_products.php
@@ -28,41 +28,64 @@
 
     <main class="container">
         <h1 class="mt-2 mb-3">Products</h1>
-        <table class="table">
-            <thead>
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Thumbnail</th>
-                    <th scope="col">Name</th>
-                    <th scope="col">Description</th>
-                    <th scope="col">Category</th>
-                    <th scope="col">Gender</th>
-                    <th scope="col">Price</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-                $sql = "SELECT products.product_id, products.name, products.description, categories.name AS category, products.gender, products.image_url, products.price FROM products, categories WHERE products.category_id=categories.category_id";
-                $result = mysqli_query($connection, $sql);
-                if (mysqli_num_rows($result) > 0) {
-                    while ($row = mysqli_fetch_assoc($result)) {
-                        echo '<tr class="">';
-                        echo '<td class="">' . $row['product_id'] . '</td>';
-                        echo '<td class=""><img src="' . $row['image_url'] . '" alt="Product Image" style="max-width: 30px;"></td>';
-                        echo '<td class="">' . $row['name'] . '</td>';
-                        echo '<td class="">' . $row['description'] . '</td>';
-                        echo '<td class="">' . $row['category'] . '</td>';
-                        echo '<td class="">' . $row['gender'] . '</td>';
-                        echo '<td class="">$' . $row['price'] . '</td>';
-                        echo '</tr>';
+        <div class="table_container">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col" class="text-nowrap">id</th>
+                        <th scope="col" class="text-nowrap">Thumbnail</th>
+                        <th scope="col" class="text-nowrap">Name</th>
+                        <th scope="col" class="text-nowrap">Description</th>
+                        <th scope="col" class="text-nowrap">Price</th>
+                        <th scope="col" class="text-nowrap">Category</th>
+                        <th scope="col" class="text-nowrap">Available Colors</th>
+                        <th scope="col" class="text-nowrap">Gender</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    $sql = "SELECT 
+                            p.product_id,
+                            p.image_url,
+                            p.name AS product_name,
+                            p.description,
+                            p.price,
+                            cat.name AS category,
+                            GROUP_CONCAT(c.name ORDER BY c.name ASC SEPARATOR ', ') AS colors,
+                            p.gender
+                        FROM 
+                            products p
+                        JOIN 
+                            categories cat ON p.category_id = cat.category_id
+                        JOIN 
+                            productcolors pc ON p.product_id = pc.product_id
+                        JOIN 
+                            colors c ON pc.color_id = c.color_id
+                        GROUP BY 
+                            p.product_id;";
+                    $result = mysqli_query($connection, $sql);
+                    if (mysqli_num_rows($result) > 0) {
+                        while ($row = mysqli_fetch_assoc($result)) {
+                            echo '<tr>';
+                            echo '<td class="text-nowrap">' . $row['product_id'] . '</td>';
+                            echo '<td class="text-nowrap"><img src="' . $row['image_url'] . '" alt="Product Image" style="max-width: 30px;"></td>';
+                            echo '<td class="text-nowrap">' . $row['product_name'] . '</td>';
+                            echo '<td class="text-nowrap">' . $row['description'] . '</td>';
+                            echo '<td class="text-nowrap">$' . $row['price'] . '</td>';
+                            echo '<td class="text-nowrap">' . $row['category'] . '</td>';
+                            echo '<td class="text-nowrap">' . $row['colors'] . '</td>';
+                            echo '<td class="text-nowrap">' . $row['gender'] . '</td>';
+                            echo '</tr>';
+                        }
+                    } else {
+                        echo "No products found";
                     }
-                } else {
-                    echo "No products found";
-                }
 
-                ?>
-            </tbody>
-        </table>
+                    ?>
+                </tbody>
+            </table>
+        </div>
+
     </main>
     <?php
     include "inc/footer.inc.php";
