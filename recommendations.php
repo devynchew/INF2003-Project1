1779f43788dba5a614601f5fb36bc8ebccf9bbf3
Dominic Loh <dominicloh1881@gmail.com>
2024-10-13 10:18:07 +0800

    dup on rec page fixed, to fix tooltip

--- recommendations.php
+++ recommendations.php
@@ -7,7 +7,7 @@ require_once 'session_config.php';
 <body>
     <?php
     session_start();
-    $user_id = isset($_SESSION['user_id']);
+    //$user_id = isset($_SESSION['user_id']);
     include "inc/head.inc.php";
     include "inc/header.inc.php";
     include "inc/nav.inc.php";
@@ -22,109 +22,6 @@ require_once 'session_config.php';
     // Create connection
     $connection = mysqli_connect($servername, $username, $password, $dbname);
 
-    // Function to get the highest clicked category ID for a user
-    function getHighestClickedCategory($connection, $user_id)
-    {
-        $category_query = "
-        SELECT p.category_id
-        FROM clicks cl
-        JOIN products p ON cl.product_id = p.product_id
-        WHERE cl.user_id = $user_id
-        GROUP BY p.category_id
-        ORDER BY SUM(cl.click_count) DESC
-        LIMIT 1";
-        $category_result = $connection->query($category_query);
-        return ($category_result->num_rows > 0) ? $category_result->fetch_assoc()['category_id'] : null;
-    }
-
-    // Function to display 3 products from a specified category
-    function displayCategoryProducts($connection, $category_id)
-    {
-        if ($category_id) {
-            $category_products_query = "
-            SELECT p.product_id, 
-            p.name AS productname, 
-            p.price, p.image_url, p.gender
-            FROM products p
-            WHERE p.category_id = $category_id
-            ORDER BY RAND()
-            LIMIT 3";
-            $category_products_result = $connection->query($category_products_query);
-
-            echo '<div class="row">';
-            while ($product = $category_products_result->fetch_assoc()) {
-                echo '<div class="col-md-4 mb-4">';
-                echo '<div class="card">';
-                echo '<img src="' . $product['image_url'] . '" class="card-img-top" alt="Product Image">';
-                echo '<div class="card-body">';
-                echo '<h2 class="card-subtitle" style="font-size: 12px; text-decoration:none;">' . $product['gender'] . '</h2>';
-                echo '<h2 class="card-title" style="font-size: 16px;">' . $product['productname'] . '</h2>';
-                echo '<p class="card-text">$' . $product['price'] . '</p>';
-                echo '<a href="product_details.php?id=' . $product['product_id'] . '" class="btn btn-primary" style="background-color: #0056b3">View Details</a>';
-                echo '</div></div></div>';
-            }
-            echo '</div>';
-        }
-    }
-
-    // Function to get the highest clicked color ID for a user
-    function getHighestClickedColor($connection, $user_id)
-    {
-        $color_query = "
-        SELECT pc.color_id
-        FROM clicks cl
-        JOIN products p ON cl.product_id = p.product_id
-        JOIN productcolors pc ON p.product_id = pc.product_id
-        WHERE cl.user_id = $user_id
-        GROUP BY pc.color_id
-        ORDER BY SUM(cl.click_count) DESC
-        LIMIT 1";
-        $color_result = $connection->query($color_query);
-        return ($color_result->num_rows > 0) ? $color_result->fetch_assoc()['color_id'] : null;
-    }
-
-    function getHighestClickedProd($connection, $user_id)
-    {
-        $prod_query = "
-        SELECT p.name
-        FROM clicks cl
-        JOIN products p ON cl.product_id = p.product_id
-        WHERE cl.user_id = $user_id
-        GROUP BY p.product_id
-        ORDER BY SUM(cl.click_count) DESC
-        LIMIT 1;";
-        $prod_result = $connection->query($prod_query);
-        return ($prod_result->num_rows > 0) ? $prod_result->fetch_assoc()['name'] : null;
-    }
-
-    // Function to display 3 products with a specified color
-    function displayColorProducts($connection, $color_id)
-    {
-        if ($color_id) {
-            $color_products_query = "
-            SELECT p.product_id, p.name AS productname, p.price, image_url, p.gender
-            FROM products p
-            JOIN productcolors pc ON p.product_id = pc.product_id
-            WHERE pc.color_id = $color_id
-            ORDER BY RAND()
-            LIMIT 3";
-            $color_products_result = $connection->query($color_products_query);
-
-            echo '<div class="row">';
-            while ($product = $color_products_result->fetch_assoc()) {
-                echo '<div class="col-md-4 mb-4">';
-                echo '<div class="card">';
-                echo '<img src="' . $product['image_url'] . '" class="card-img-top" alt="Product Image">';
-                echo '<div class="card-body">';
-                echo '<h2 class="card-subtitle" style="font-size: 12px; text-decoration:none;">' . $product['gender'] . '</h2>';
-                echo '<h2 class="card-title" style="font-size: 16px;">' . $product['productname'] . '</h2>';
-                echo '<p class="card-text">$' . $product['price'] . '</p>';
-                echo '<a href="product_details.php?id=' . $product['product_id'] . '" class="btn btn-primary" style="background-color: #0056b3">View Details</a>';
-                echo '</div></div></div>';
-            }
-            echo '</div>';
-        }
-    }
     // Check connection
     if (!$connection) {
         echo "<script>console.error('Connection failed: " . mysqli_connect_error() . "');</script>";
@@ -136,7 +33,116 @@ require_once 'session_config.php';
     <main>
         <div class="container">
             <?php
+            $displayed_product_ids = [];
             $user_id = isset($_SESSION['user_id']);
+            // Function to get the highest clicked category ID for a user
+            function getHighestClickedCategory($connection, $user_id)
+            {
+                $category_query = "
+                SELECT p.category_id
+                FROM clicks cl
+                JOIN products p ON cl.product_id = p.product_id
+                WHERE cl.user_id = $user_id
+                GROUP BY p.category_id
+                ORDER BY SUM(cl.click_count) DESC
+                LIMIT 1";
+                $category_result = $connection->query($category_query);
+                return ($category_result->num_rows > 0) ? $category_result->fetch_assoc()['category_id'] : null;
+            }
+
+            // Function to display 3 products from a specified category
+            function displayCategoryProducts($connection, $category_id)
+            {
+                if ($category_id) {
+                    $category_products_query = "
+                    SELECT p.product_id, 
+                    p.name AS productname, 
+                    p.price, p.image_url, p.gender
+                    FROM products p
+                    WHERE p.category_id = $category_id
+                    ORDER BY RAND()
+                    LIMIT 3";
+                    $category_products_result = $connection->query($category_products_query);
+
+                    echo '<div class="row">';
+                    while ($product = $category_products_result->fetch_assoc()) {
+                        $displayed_product_ids[] = $product['product_id'];
+                        echo '<div class="col-md-4 mb-4">';
+                        echo '<div class="card">';
+                        echo '<img src="' . $product['image_url'] . '" class="card-img-top" alt="Product Image">';
+                        echo '<div class="card-body">';
+                        echo '<h2 class="card-subtitle" style="font-size: 12px; text-decoration:none;">' . $product['gender'] . '</h2>';
+                        echo '<h2 class="card-title" style="font-size: 16px;">' . $product['productname'] . '</h2>';
+                        echo '<p class="card-text">$' . $product['price'] . '</p>';
+                        echo '<a href="product_details.php?id=' . $product['product_id'] . '" class="btn btn-primary" style="background-color: #0056b3">View Details</a>';
+                        echo '</div></div></div>';
+                    }
+                    echo '</div>';
+                }
+                return $displayed_product_ids;
+            }
+
+            // Function to get the highest clicked color ID for a user
+            function getHighestClickedColor($connection, $user_id, $displayed_product_ids)
+            {
+                $color_query = "
+                SELECT pc.color_id
+                FROM clicks cl
+                JOIN products p ON cl.product_id = p.product_id
+                JOIN productcolors pc ON p.product_id = pc.product_id
+                WHERE cl.user_id = $user_id
+                GROUP BY pc.color_id
+                ORDER BY SUM(cl.click_count) DESC
+                LIMIT 1";
+                $color_result = $connection->query($color_query);
+                return ($color_result->num_rows > 0) ? $color_result->fetch_assoc()['color_id'] : null;
+            }
+
+            function getHighestClickedProd($connection, $user_id)
+            {
+                $prod_query = "
+                SELECT p.name
+                FROM clicks cl
+                JOIN products p ON cl.product_id = p.product_id
+                WHERE cl.user_id = $user_id
+                GROUP BY p.product_id
+                ORDER BY SUM(cl.click_count) DESC
+                LIMIT 1;";
+                $prod_result = $connection->query($prod_query);
+                return ($prod_result->num_rows > 0) ? $prod_result->fetch_assoc()['name'] : null;
+            }
+
+            // Function to display 3 products with a specified color
+            function displayColorProducts($connection, $color_id, $displayed_product_ids)
+            {
+                if ($color_id) {
+                    $excluded_ids = implode(',', $displayed_product_ids) ?: '';
+                    //echo 'excluded ids: ', $excluded_ids;
+                    $color_products_query = "
+                    SELECT p.product_id, p.name AS productname, p.price, image_url, p.gender
+                    FROM products p
+                    JOIN productcolors pc ON p.product_id = pc.product_id
+                    WHERE pc.color_id = $color_id
+                    AND p.product_id NOT IN ($excluded_ids)
+                    ORDER BY RAND()
+                    LIMIT 3";
+                    $color_products_result = $connection->query($color_products_query);
+
+                    echo '<div class="row">';
+                    while ($product = $color_products_result->fetch_assoc()) {
+                        echo '<div class="col-md-4 mb-4">';
+                        echo '<div class="card">';
+                        echo '<img src="' . $product['image_url'] . '" class="card-img-top" alt="Product Image">';
+                        echo '<div class="card-body">';
+                        echo '<h2 class="card-subtitle" style="font-size: 12px; text-decoration:none;">' . $product['gender'] . '</h2>';
+                        echo '<h2 class="card-title" style="font-size: 16px;">' . $product['productname'] . '</h2>';
+                        echo '<p class="card-text">$' . $product['price'] . '</p>';
+                        echo '<a href="product_details.php?id=' . $product['product_id'] . '" class="btn btn-primary" style="background-color: #0056b3">View Details</a>';
+                        echo '</div></div></div>';
+                    }
+                    echo '</div>';
+                }
+            }
             if ($user_id) {
                 $check_clicks_query = "
                     SELECT EXISTS (
@@ -147,18 +153,20 @@ require_once 'session_config.php';
                 $result = $connection->query($check_clicks_query);
                 $row = $result->fetch_assoc();
                 if ($row['user_has_clicks'] == 1) {
-                    // User has clicked on products
+                    // User has clicked on products, to add tooltip with highest clicked product here
                     echo '<h1 class="mt-5 mb-3">Recommended Products based on your history</h1>';
-                    echo '<p class="mt-3 mb-3" style="font-size: 1.2em;">Based on your most clicked product: ' . getHighestClickedProd($connection, $user_id) . '</p>';
+                    $mostClickedProd = getHighestClickedProd($connection, $user_id);
+                    echo '<p class="mt-3 mb-3" style="font-size: 1.2em;">Based on your <span title="' 
+                    . htmlspecialchars($mostClickedProd, ENT_QUOTES) . '">most clicked product</span>: </p>';
                     echo '<div class="row">';
                     $category_id = getHighestClickedCategory($connection, $user_id);
                     //echo 'catid: ', $category_id;
-                    displayCategoryProducts($connection, $category_id);
-            
+                    $displayed_product_ids = displayCategoryProducts($connection, $category_id);
+
                     // Display products based on highest clicked color
-                    $color_id = getHighestClickedColor($connection, $user_id);
+                    $color_id = getHighestClickedColor($connection, $user_id, $displayed_product_ids);
                     //echo 'colorid: ', $color_id;
-                    displayColorProducts($connection, $color_id);
+                    displayColorProducts($connection, $color_id, $displayed_product_ids);
                     echo '</div>';
                 } else {
                     // User has no clicks recorded
