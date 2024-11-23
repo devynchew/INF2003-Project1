92c86b0729053e7c587008ed4709700abc6e47ce
Dominic Loh <dominicloh1881@gmail.com>
2024-11-23 09:35:28 +0800

    Revert "added mongoDB test files"
    This reverts commit 4c28e41b4594fc64398b1b0bfcec9a05192d8670.

--- mongotest2.php
+++
@@ -1,7 +0,0 @@
-<?php
-require 'vendor/autoload.php'; // For MongoDB library if using Composer
-
-$client = new MongoDB\Client("mongodb+srv://2301823:UsagiParkdecagram4@cluster0.ugdqd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
-
-echo "Connected to MongoDB!";
-?>
